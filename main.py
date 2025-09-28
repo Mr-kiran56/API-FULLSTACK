73a30a2cb8ab8d346df132582b51716e2d7497a5
Mr-kiran56 <kiranpunna815@gmail.com>
2025-09-28 16:26:33 +0530

    wrqww

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from routes import posts,users,login
+from models import Base
+from database import engine
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
 
 app.include_router(posts.router)
 app.include_router(users.router)
